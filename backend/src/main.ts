c69d44a7b329bde90472387628dffaa99b0c0397
Ira Ryzha <ira.ryzha2803@gmail.com>
2024-12-22 18:46:37 +0200

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://flash-sigma-ten.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://social-app-three-sage.vercel.app',
+    ],
     credentials: true,
   });
 
