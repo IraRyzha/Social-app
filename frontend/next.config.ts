e60417af41439f29410de141da3a40287470ce79
Ira Ryzha <ira.ryzha2803@gmail.com>
2024-12-22 17:54:51 +0200

    ignore eslint

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["randomuser.me", "example.com", "via.placeholder.com"], // Додаємо randomuser.me до дозволених хостів
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Вимкне ESLint під час збірки
+  },
 };
 
 export default nextConfig;
